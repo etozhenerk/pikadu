4495e6c8012be374f3eac731329b0631d2e0d280
etozhenerk <etozhenerk@yandex.ru>
2021-03-11 23:20:31 +0300

    media + add-post

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const menuToggle = document.getElementById("menu-toggle"),
+  menu = document.querySelector(".sidebar");
+
+menuToggle.addEventListener("click", (e) => {
+  e.preventDefault();
+  menu.classList.toggle('visible');
+});
